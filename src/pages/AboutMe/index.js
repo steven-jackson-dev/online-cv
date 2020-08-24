dba357b8b70c8859b9d65a8b0e847feea60d7e20
Steven <stevenjackson-dev@gmail.com>
2020-08-24 17:42:22 +0200

    [updates] updated projects and about page with new projects and cv and cover letter links. prettier linting update

--- src/pages/AboutMe/index.js
+++ src/pages/AboutMe/index.js
@@ -1,72 +1,115 @@
-import React, { useEffect } from 'react'
-import { Row, Container, Col, Image } from 'react-bootstrap'
+import React, { useEffect } from "react";
+import { Row, Container, Col, Image } from "react-bootstrap";
 import { BounceInRight } from "react-spring-pop";
-import setupImage from 'assets/images/my_setup.jpg'
-import resume from 'assets/other/Steven Jackson CV.docx'
-import FontAwesome from 'react-fontawesome'
-import './AboutMe.scss'
+import setupImage from "assets/images/my_setup.jpg";
+import FontAwesome from "react-fontawesome";
+import "./AboutMe.scss";
 
 const AboutMe = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
-    useEffect(() => {
-        window.scrollTo(0, 0);
-    }, [])
+  return (
+    <section className="AboutMe py-5">
+      <Container>
+        <Row>
+          <Col md={3} xs={12} lg={3}>
+            <div className="title text-center">
+              <h3>ABOUT ME</h3>
+            </div>
+          </Col>
+          <Col md={9} xs={12} lg={9} className="content">
+            <BounceInRight>
+              <div className="mb-5 aboutMe-contactDetails">
+                <h2>Contact Details</h2>
+                <ul>
+                  <li>
+                    <FontAwesome name="phone-square" /> +2776 997 5180
+                  </li>
+                  <li>
+                    <FontAwesome name="envelope" /> Stevenjackson.dev@gmail.com
+                  </li>
+                </ul>
+                <div className="aboutMe-downloadCv">
+                  <a
+                    style={{ marginRight: "10px" }}
+                    href="https://script-kiddie.co.za/external-images/Steven-Jackson-CV-2020.docx"
+                  >
+                    Download CV{" "}
+                    <FontAwesome className="arrow-down" name="download" />
+                  </a>
+                  <a href="https://script-kiddie.co.za/external-images/Steven-Jackson-Cover-Letter.docx">
+                    Cover Letter{" "}
+                    <FontAwesome className="arrow-down" name="download" />
+                  </a>
+                </div>
+              </div>
+              <div className="mb-5">
+                <h2>General</h2>
+                <p className="mt-3">
+                  I am a very calm and happy person. I like to make jokes,
+                  banter and prank people in a hilarious way. I like to help
+                  people and I am very good at explaining technical jargon to
+                  non-technical people using analogies and situations. I take
+                  pride in my work and coding is not just a career for me, it is
+                  also a passion and hobby. I love creating functionality and
+                  making things work while keeping the user experience in mind.
+                  I will not stop coding till I know as much as I possibily can
+                  in FED and BED.
+                </p>
+              </div>
+              <div className="mb-5">
+                <h2>Personality Type</h2>
+                <p>
+                  <b>
+                    <em>INTJ (The Architect)</em>
+                  </b>
+                </p>
+                <p>
+                  I am not a believer in online personality tests, but this test
+                  hit the nail on the head and I highly recommend it.
+                </p>
+                <blockquote className="blockquote mt-3">
+                  <small className="mb-2">
+                    <i>
+                      "It can be lonely at the top. Being one of the rarest
+                      personality types and being among the most capable people,
+                      Architects know this all too well. They make up just two
+                      percent of the population, and women with this personality
+                      type are especially rare, forming only 0.8%. It can be
+                      difficult for Architects to find people who can keep up
+                      with their non-stop analysis of things. People with this
+                      personality type are imaginative yet decisive... ambitious
+                      yet like their privacy... curious about everything but
+                      remain focused."
+                    </i>
+                  </small>
+                  <span className="blockquote-footer">
+                    <cite title="16personalities.com">16personalities.com</cite>
+                  </span>
+                </blockquote>
+              </div>
+              <div className="mb-5">
+                <h2>Interests</h2>
+                <p>I am a big fan of anime, coding, researching and gaming</p>
+                <p>
+                  My free time is spent watching anime, gaming and
+                  learning/researching something new. The majority of my day is
+                  working or communicating with clients to make sure they
+                  receive the best Experience
+                </p>
+              </div>
+              <div className="mb-5">
+                <h2>My Setup</h2>
+                <Image src={setupImage} fluid thumbnail />
+              </div>
+            </BounceInRight>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
 
-    return (
-        <section className="AboutMe py-5">
-            <Container>
-                <Row>
-                    <Col md={3} xs={12} lg={3}>
-                        <div className="title text-center">
-                            <h3>ABOUT ME</h3>
-                        </div>
-                    </Col>
-                    <Col md={9} xs={12} lg={9} className="content">
-                        <BounceInRight>
-                        <div className="mb-5 aboutMe-contactDetails">
-                                <h2>Contact Details</h2>
-                                <ul>
-                                        <li><FontAwesome name="phone-square" /> +27769975180</li>
-                                        <li><FontAwesome name="envelope" /> Stevenjackson.dev@gmail.com</li>
-                                    </ul>
-                                    <div className="aboutMe-downloadCv">
-                                        <a href={resume}>Download CV <FontAwesome className="arrow-down" name="download" /></a>
-                                    </div>
-                            </div>
-                            <div className="mb-5">
-                                <h2>General</h2>
-                                <p className="mt-3">
-                                    I am a very calm and happy person. I like to make jokes, banter and prank people in a hilarious way.
-                                    I like to help people and I am very good at explaining technical jargon to non-technical people using analogies and situations.
-                                    I take pride in my work and coding is not just a career for me, it is also a passion and hobby. I love creating functionality and making things work while keeping the user experience in mind.
-                                    I will not stop coding till I know as much as I possibily can in FED and BED.
-                                </p>
-                            </div>
-                            <div className="mb-5">
-                                <h2>Personality Type</h2>
-                                <p><b><em>INTJ (The Architect)</em></b></p>
-                                <p>I am not a believer in online personality tests, but this test hit the nail on the head and I highly recommend it.</p>
-                                <blockquote className="blockquote mt-3">
-                                    <small className="mb-2"><i>"It can be lonely at the top. Being one of the rarest personality types and being among the most capable people, Architects know this all too well. They make up just two percent of the population, and women with this personality type are especially rare, forming only 0.8%. It can be difficult for Architects to find people who can keep up with their non-stop analysis of things. People with this personality type are imaginative yet decisive... ambitious yet like their privacy... curious about everything but remain focused."</i></small>
-                                    <span className="blockquote-footer"><cite title="16personalities.com">16personalities.com</cite></span>
-                                </blockquote>
-                            </div>
-                            <div className="mb-5">
-                                <h2>Interests</h2>
-                                <p>I am a big fan of anime, coding, researching and gaming</p>
-                                <p>My free time is spent watching anime, gaming and learning/researching something new. The majority of my day is working or communicating with clients to make sure they receive the best Experience</p>
-
-                            </div>
-                            <div className="mb-5">
-                                <h2>My Setup</h2>
-                                <Image src={setupImage} fluid thumbnail />
-                            </div>
-                        </BounceInRight>
-                    </Col>
-                </Row>
-            </Container>
-        </section >
-    )
-}
-
-export default AboutMe
+export default AboutMe;
--- src/pages/Projects/index.js
+++ src/pages/Projects/index.js
@@ -1,79 +1,146 @@
-import React, { useEffect } from 'react'
-import { Row, Container, Col } from 'react-bootstrap'
-import AppPageDivider from 'components/AppPageDivider'
+import React, { useEffect } from "react";
+import { Row, Container, Col } from "react-bootstrap";
+import AppPageDivider from "components/AppPageDivider";
 import { BounceInRight } from "react-spring-pop";
-import './Projects.scss'
-import FontAwesome from 'react-fontawesome';
+import "./Projects.scss";
+import FontAwesome from "react-fontawesome";
 const Projects = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
-    useEffect(() => {
-        window.scrollTo(0, 0);
-    }, [])
+  return (
+    <section id="Projects" className="py-5 mt-5">
+      <Container>
+        <Row>
+          <Col md={3} xs={12} lg={3}>
+            <div className="title text-center">
+              <h3>Projects</h3>
+            </div>
+          </Col>
+          <Col md={9} xs={12} lg={9} className="content">
+            <BounceInRight>
+              <div className="mb-5">
+                <h2>Script Kid - Github Alias</h2>
+                <p className="mt-3 mb-5">
+                  A separate personal repo that I use for uploading code that is
+                  reusable in combination with Script Kiddie.
+                  <br />
+                </p>
+                <a
+                  href="https://github.com/script-kid-official/"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  View Script Kid Repo
+                  <FontAwesome className="arrow-down" name="arrow-right" />
+                </a>{" "}
+                <br />
+              </div>
+              <AppPageDivider />
+              <div className="mb-5">
+                <h2>Script Kiddie</h2>
+                <p className="mt-3 mb-5">
+                  A personal app for storing useful information and snippets for
+                  HTML 5, CSS 3, PHP, Javascript, jQuery, Wordpress, React,
+                  Bootstrap, SCSS, Theory, ect.
+                  <br />
+                </p>
+                <a
+                  className="mt-3 mb-5"
+                  href="http://script-kiddie.co.za/"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  Script Kiddie - Wordpress API
+                  <FontAwesome className="arrow-down" name="arrow-right" />
+                </a>{" "}
+                <br />
+                <br />
+                <br />
+                <a
+                  className="mt-3 mb-5"
+                  href="http://script-kiddie.co.za/app"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  Script Kiddie React App
+                  <FontAwesome className="arrow-down" name="arrow-right" />
+                </a>{" "}
+                <br />
+              </div>
+              <AppPageDivider />
+              <div className="mb-5">
+                <h2>Github User and Repo Search</h2>
+                <p className="mt-3 mb-5">
+                  A basic App that allows you to search through Github Users and
+                  Repositories.
+                </p>
+                <a
+                  href="https://steven-jackson-dev.github.io/react-github-search/"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  View Github User and Repo Search{" "}
+                  <FontAwesome className="arrow-down" name="arrow-right" />
+                </a>
+              </div>
+              <AppPageDivider />
+              <div className="mb-5">
+                <h2>Covid 19 Tracker</h2>
+                <p className="mt-3 mb-5">
+                  A Covid Tracking App to track the status of the infection
+                </p>
+                <a
+                  href="https://steven-jackson-dev.github.io/covid-tracker/"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  View Covid Tracker App
+                  <FontAwesome className="arrow-down" name="arrow-right" />
+                </a>
+              </div>
+              <AppPageDivider />
+              <div className="mb-5">
+                <h2>React Boilerplate with Bootstrap</h2>
+                <em>Boilerplate Template</em>
+                <p className="mt-3 mb-5">
+                  Scaffold React App with common packages using React Bootstrap
+                  as the CSS Framework
+                </p>
+                <a
+                  href="https://steven-jackson-dev.github.io/react-bootstrap-boilerplate/"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  Bootstrap Boilerplate
+                  <FontAwesome className="arrow-down" name="arrow-right" />
+                </a>
+              </div>
+              <AppPageDivider />
 
-    return (
-        <section id="Projects" className="py-5 mt-5">
-            <Container>
-                <Row>
-                    <Col md={3} xs={12} lg={3}>
-                        <div className="title text-center">
-                            <h3>Projects</h3>
-                        </div>
-                    </Col>
-                    <Col md={9} xs={12} lg={9} className="content">
-                        <BounceInRight>
-                            <div className="mb-5">
-                                <h2>Script Kiddie</h2>
-                                <em>Beta</em>
-                                <p className="mt-3 mb-5">
-                                    A personal app for storing useful information and snippets for HTML 5, CSS 3, PHP. Javascript, jQuery, React, Bootstrap, ect. I plan to make it public once it comes out of beta.<br />
-                                    <b><em>Test Logins are pre-entered for demo purposes.</em></b>
-                                </p>
-                                <a href="http://script-kiddie.co.za/" rel="noopener noreferrer" target="_blank">Script Kiddie App<FontAwesome className="arrow-down" name="arrow-right" /></a> <br />
-                            </div>
-                            <AppPageDivider />
-                            <div className="mb-5">
-                                <h2>Github User and Repo Search</h2>
-                                <p className="mt-3 mb-5">
-                                    A basic App that allows you to search through Github Users and Repositories.
-                            </p>
-                                <a href="https://steven-jackson-dev.github.io/react-github-search/" rel="noopener noreferrer" target="_blank">View Github User and Repo Search <FontAwesome className="arrow-down" name="arrow-right" /></a>
-                            </div>
-                            <AppPageDivider />
-                            <div className="mb-5">
-                                <h2>Covid 19 Tracker</h2>
-                                <p className="mt-3 mb-5">
-                                    A Covid Tracking App to track the status of the infection
-                            </p>
-                                <a href="https://steven-jackson-dev.github.io/covid-tracker/" rel="noopener noreferrer" target="_blank">View Covid Tracker App<FontAwesome className="arrow-down" name="arrow-right" /></a>
-                            </div>
-                            <AppPageDivider />
-                            <div className="mb-5">
-                                <h2>React Boilerplate with Bootstrap</h2>
-                                <em>Boilerplate Template</em>
-                                <p className="mt-3 mb-5">
-                                    Scaffold React App with common packages using React Bootstrap as the CSS Framework
-                            </p>
-                                <a href="https://steven-jackson-dev.github.io/react-bootstrap-boilerplate/" rel="noopener noreferrer" target="_blank">Bootstrap Boilerplate<FontAwesome className="arrow-down" name="arrow-right" /></a>
-                            </div>
-                            <AppPageDivider />
+              <div className="mb-5">
+                <h2>React Boilerplate with Material</h2>
+                <em>Boilerplate Template</em>
+                <p className="mt-3 mb-5">
+                  Scaffold React App with common packages using Material UI as
+                  the CSS Framework
+                </p>
+                <a
+                  href="https://steven-jackson-dev.github.io/react-material-boilerplate/"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  Material Boilerplate
+                  <FontAwesome className="arrow-down" name="arrow-right" />
+                </a>
+              </div>
+            </BounceInRight>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
 
-                            <div className="mb-5">
-                                <h2>React Boilerplate with Material</h2>
-                                <em>Boilerplate Template</em>
-                                <p className="mt-3 mb-5">
-                                    Scaffold React App with common packages using Material UI as the CSS Framework
-                            </p>
-                                <a href="https://steven-jackson-dev.github.io/react-material-boilerplate/" rel="noopener noreferrer" target="_blank">Material Boilerplate<FontAwesome className="arrow-down" name="arrow-right" /></a>
-                            </div>
-
-                        </BounceInRight>
-                    </Col>
-
-                </Row>
-            </Container>
-
-        </section>
-    )
-}
-
-export default Projects
+export default Projects;
